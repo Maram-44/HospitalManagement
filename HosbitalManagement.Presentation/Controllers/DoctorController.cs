d240ff44b766a32d2e91387bc5516c3171220944
Maram-44 <maramaljoohy@gmail.com>
2025-10-03 09:39:29 +0300

    Presentation Layer

---
+++ HosbitalManagement.Presentation/Controllers/DoctorController.cs
@@ -0,0 +1,88 @@
+﻿using HospitalManagement.BussinessLogic.InterfacesServices;
+using HospitalManagement.BussinessLogic.ModelView;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HosbitalManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IDoctorServices _services;
+        public DoctorController(IDoctorServices services)
+        {
+            _services = services;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult CreateDoctor(AddDoctorVM doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                _services.AddDoctor(doctor);
+                return Ok();
+            }
+            return BadRequest();    
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult GetDoctorById(int id)
+        {
+            var doctor= _services.GetDoctorById(id);
+            if(doctor == null)
+            {
+                return NotFound();
+            }
+            return Ok(doctor);
+        }
+
+        [HttpGet]
+        [Route("")]
+        public ActionResult GetAllDoctors()
+        {
+            var doctors= _services.GetAllDoctors();
+            return Ok(doctors);
+        }
+
+        [HttpGet]
+        [Route("department/{departmentId}")]
+        public ActionResult GetDoctorsByDepartment(int departmentId) 
+        {
+            var doctors=_services.GetDoctorsByDepartment(departmentId);
+            if (doctors == null)
+                return NotFound();
+
+            return Ok(doctors);
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public ActionResult DeleteDoctor(int id)
+        {
+            var doctor=_services.GetDoctorById(id);
+            if(doctor == null)
+                return NotFound();
+            _services.DeleteDoctor(id);
+            return Ok();
+
+        }
+
+        [HttpPut]
+        [Route("")]
+        public ActionResult UpdateDoctor(DoctorVM doctorVM)
+        {
+            var doctor=_services.GetDoctorById(doctorVM.Id);
+
+            if(doctor == null)
+                return NotFound();
+
+            _services.UpdateDoctor(doctorVM);
+
+            return Ok();
+        }
+    }
+}
---
+++ HosbitalManagement.Presentation/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using HospitalManagement.BussinessLogic.InterfacesServices;
+using HospitalManagement.BussinessLogic.ModelView;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace HosbitalManagement.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public UserController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        [Route("auth")]
+        public ActionResult<string>AuthenticateUser(AuthenticationRequest request)
+        {
+            var response = _authService.AuthenticateRequest(request);
+            if(response == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(response);
+        }
+    }
+}
